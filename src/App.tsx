d68a53815ed10f7e218e449dade173f1b45c7c0f
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2020-10-12 15:39:55 -0300

    Page navigation and routes page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
-import './styles/pages/landing.css'
-
-import logoImg from './images/logo.svg'
+import Routes from './routes';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div id="page-landing" className="App">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="" />
-
-        <main>
-          <h1>Leve felicidade</h1>
-          <p>Visite orfanatos</p>
-        </main>
-
-        <div className="location">
-          <strong>Maringá</strong>
-          <span>Parana</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
