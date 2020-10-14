6d0e9624bf6a3b18cd052dd29e50a0eb527cb634
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2020-10-14 10:58:03 -0300

    Component abstraction

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -1,36 +1,17 @@
 import React from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import { useHistory } from "react-router-dom";
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
-
-import mapMarkerImg from '../images/map-marker.svg';
+import { FiPlus } from "react-icons/fi";
 
 import '../styles/pages/create-orphanage.css';
+import Sidebar from "../components/Sidebar";
 
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
+import happyMapIcon from '../utils/mapIcon';
 
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
-
-export default function CreateOrphanage() {
-  const { goBack } = useHistory();
-
+export default function CreateOrphanage() {  
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <form className="create-orphanage-form">
